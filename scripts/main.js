fb354fbc3ca99e1de1406d18d5265b6381898f25
HudsonS <hudsonshaeffer@gmail.com>
2023-04-27 12:25:14 -0400

    boredAPI request

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,33 @@
-const searchBox = document.getElementById('top-search')
+const searchBox = document.getElementById('top-search');
+const resultElem = document.getElementById('activity');
+const musicElem = document.getElementById('music');
+const activityQueryURLBase = "http://www.boredapi.com/api/activity?type=";
+let activityQueryURL;
+let activity;
+
+//searchbox listener
 searchBox.onsubmit = (ev) => {
-  console.log('submitted top-search with', ev)
-  // https://stackoverflow.com/a/26892365/1449799
-  const formData = new FormData(ev.target)
-  console.log(formData)
-  for (const pair of formData.entries()) {
-    console.log(`${pair[0]}, ${pair[1]}`);
+  ev.preventDefault();
+  let activityType = ev.srcElement.children[0].value.toLowerCase(); // get search input
+  ev.srcElement.children[0].value = '';                             // clear search input
+  activityQueryURL = activityQueryURLBase + activityType;           // assemble boredAPI request
+
+  //bored api request
+  if (activityType) { 
+    boredapiRequest.open("GET", `${activityQueryURL}`);
+    boredapiRequest.send();
   }
-  ev.preventDefault()
+  // setTimeout(() => {console.log(activity)}, 25);
+
 }
+
+const boredapiRequest = new XMLHttpRequest();
+boredapiRequest.addEventListener("load", function (ev) { //step 1
+  const structuredData = JSON.parse(ev.target.responseText);
+  activity = structuredData.activity;
+  if (activity) {
+    resultElem.innerText = activity + "!";
+  } else {
+    resultElem.innerText = "Oops! That's not a valid type!";
+  }
+});
